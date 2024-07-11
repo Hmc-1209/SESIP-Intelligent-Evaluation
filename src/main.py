3b377162b922b344e267836eddff4a481433252b
何旻臻 Min Chen Ho <minchenho@iii.org.tw>
2024-07-11 15:42:36 +0800

    layout adjust

--- src/main.py
+++ src/main.py
@@ -2,20 +2,24 @@ import gradio as gr
 import fitz
 
 
-def upload_file(file_path):
+def upload_file(file):
     text = ""
-    with fitz.open(file_path) as doc:
+    with fitz.open(file) as doc:
         for page in doc:
             text += page.get_text()
     return text
 
 
-demo = gr.Interface(
-    title="SESIP Intelligence Eval",
-    description="Fast LLM evaluated result will be provide using this tool.",
-    fn=upload_file,
-    inputs=[gr.File(label="Upload ST")],
-    outputs=[gr.Textbox(label="PDF content", lines=10)],
-)
+with gr.Blocks() as demo:
+    gr.Markdown("# SESIP Intelligence Eval")
+    gr.Markdown("Fast LLM evaluated result will be provided using this tool.")
+
+    with gr.Column():
+        with gr.Row():
+            file_input = gr.File(label="Upload PDF")
+            file_detail = gr.Markdown("## Security Target details", height=500)
+        pdf_content = gr.Textbox(label="PDF content", lines=10)
+
+    file_input.upload(upload_file, file_input, pdf_content)
 
 demo.launch()
