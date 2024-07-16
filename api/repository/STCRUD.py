9cc2fa6dc1753c88a7d676ddf330f9301fc2db07
Sally Lin <LS0723000@gmail.com>
2024-07-16 20:29:49 +0800

    FEAT: Add endpoint to delete st

--- api/repository/STCRUD.py
+++ api/repository/STCRUD.py
@@ -22,3 +22,8 @@ async def update_st_by_id(st_id: int, new_st: UpdateST) -> bool:
     stmt = SecurityTarget.update().where(SecurityTarget.c.st_id == st_id).values(st_name=new_st.st_name,
                                                                                  owner_id=new_st.owner_id)
     return await execute_stmt_in_tran([stmt])
+
+
+async def delete_st_by_id(st_id: int) -> bool:
+    stmt = SecurityTarget.delete().where(SecurityTarget.c.st_id == st_id)
+    return await execute_stmt_in_tran([stmt])
--- api/router/st.py
+++ api/router/st.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from schemas import DetailST, UpdateST
 from exception import duplicate_data, bad_request, no_such_st, st_not_belongs
-from repository.STCRUD import get_st_by_id, get_st_by_name, get_st_by_user_id, update_st_by_id
+from repository.STCRUD import get_st_by_id, get_st_by_name, get_st_by_user_id, update_st_by_id, delete_st_by_id
 from authentication.JWTtoken import get_current_user
 
 router = APIRouter(prefix="/st", tags=["Security Target"])
@@ -30,4 +30,17 @@ async def update_security_target(st_id: int, new_st: UpdateST, current_user=Depe
     update = UpdateST.model_validate(st).model_copy(update=update_data)
 
     if not await update_st_by_id(st_id, update):
+        raise bad_request
+
+
+@router.delete("/{st_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_security_target(st_id: int, current_user=Depends(get_current_user)) -> None:
+    st = await get_st_by_id(st_id)
+    if not st:
+        raise no_such_st
+
+    if not current_user.user_id == st.owner_id:
+        raise st_not_belongs
+
+    if not await delete_st_by_id(st_id):
         raise bad_request
