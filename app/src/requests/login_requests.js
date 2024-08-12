c7c450585a009108babee706ee93c5e03ad8cafe
何旻臻 Min Chen Ho <minchenho@iii.org.tw>
2024-08-12 16:38:38 +0800

    REFACTOR: frontend request comment and code refactor

--- app/src/requests/login_requests.js
+++ app/src/requests/login_requests.js
@@ -2,109 +2,115 @@ import axios from "axios";
 import { ip } from "./request_host_ip";
 
 
-/* Request behaviors with access token */
 const get_access_token = async (user_name, user_password) => {
-    // Get the access token for user to verify credential
+  // Get the access token for user to do actions.
+  //  Param: 
+  //    user_name: The user's username for login.
+  //    user_password: The user's password for login.
+  //  Return:
+  //    INTEGER: Status for frontend to take actions.
+  //    response.data.detail: Detail information for frontend to take actions.
   
-    const formData = new FormData();
-    formData.append("username", user_name);
-    formData.append("password", user_password);
+  const formData = new FormData();
+  formData.append("username", user_name);
+  formData.append("password", user_password);
   
-    try {
-      const response = await axios.post(`${ip}/token`, formData, {
+  try {
+    const response = await axios.post(`${ip}/token`, formData, {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      validateStatus: function (status) {
+        return (status >= 200 && status < 300) || status === 404;
+      },
+    });
+    if (response.data.access_token) {
+      window.localStorage.setItem("access_token", response.data.access_token);
+      return 1;
+    } else {
+      return response.data.detail;
+    }
+  } catch (error) {
+    return 5;
+  }
+};
+export default get_access_token;
+
+
+export const validate_access_token = async (
+    access_token = window.localStorage.getItem("access_token")
+  ) => {
+  // Verify if the access token still not expired
+  //  Param: 
+  //   access_token: The user's access_token to see if it is still available.
+  //  Return:
+  //    BOOL: Status for frontend to take actions.
+  //    response: Detail information for frontend to take actions.
+
+  try {
+    const response = await axios.post(`${ip}/token/validate_access_token?token=${access_token}`, {
         headers: {
           accept: "application/json",
           "Content-Type": "application/x-www-form-urlencoded",
         },
         validateStatus: function (status) {
-          return (status >= 200 && status < 300) || status === 404;
+          return status >= 200 && status < 300;
         },
-      });
-      if (response.data.access_token) {
-        window.localStorage.setItem("access_token", response.data.access_token);
-        return 1;
-      } else {
-        return response.data.detail;
       }
-    } catch (error) {
-      console.log(error);
+    );
+    if (response.status === 200) {
+      return response;
     }
-    return 5;
-};
-export default get_access_token;
-
-
-/* Request behaviors for validating access token */
-export const validate_access_token = async (
-    access_token = window.localStorage.getItem("access_token")
-  ) => {
-    // Verify if the access token is not expired
-    try {
-      const response = await axios.post(
-        `${ip}/token/validate_access_token?token=${access_token}`,
-        {
-          headers: {
-            accept: "application/json",
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          validateStatus: function (status) {
-            return status >= 200 && status < 300;
-          },
-        }
-      );
-      if (response.status === 200) {
-        return response;
-      }
-      return response.detail;
-    } catch (error) {
-      if (error.response && error.response.status === 403) {
-        console.log(error.response.data);
-      }
-    }
+    return response.detail;
+  } catch (error) {
     return false;
+  }
 };
 
 
-/* Resuest behaviors for sign up */
 export const regist_new_user = async (
     user_name,
     user_password,
     user_password_confirm
   ) => {
-    // Regist new user using given user_name and password
-    if (user_password !== user_password_confirm) {
-      return 3;
-    }
+  // Verify if the access token still not expired
+  //  Param: 
+  //   user_name: The new user's username.
+  //   user_password: The new user's password.
+  //   user_password_confirm: The new user's password, for confirmation usage.
+  //  Return:
+  //    INTEGER, Detail information for frontend to take actions.
+    
+  if (user_password !== user_password_confirm) {
+    return 3;
+  }
   
-    const body = {
-      username: user_name,
-      password: user_password,
-    };
-    console.log(JSON.stringify(body))
+  const body = {
+    username: user_name,
+    password: user_password,
+  };
   
-    try {
-      const response = await axios.post(
-        `${ip}/user`,
-        JSON.stringify(body),
-        {
-          headers: {
-            accept: "application/json",
-            "Content-Type": "application/json",
-          },
-  
-          validateStatus: function (status) {
-            return (status >= 200 && status < 300) || status === 400;
-          },
-        }
-      );
-      if (response.status === 201) {
-        return response.status;
-      } else if (response.data.detail === "Data Duplicated.") {
-        return "Data Duplicated.";
+  try {
+    const response = await axios.post(`${ip}/user`, JSON.stringify(body), {
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        validateStatus: function (status) {
+          return (status >= 200 && status < 300) || status === 400;
+        },
       }
-    } catch (error) {
-      if (error.response && error.response.status === 403) {
-        return 5;
-      }
+    );
+    
+    if (response.status === 201) {
+      return response.status;
+    } else if (response.data.detail === "Data Duplicated.") {
+      return "Data Duplicated.";
     }
+  } catch (error) {
+    if (error.response && error.response.status === 403) {
+      return 5;
+    }
+  }
 };
--- app/src/requests/user_requests.js
+++ app/src/requests/user_requests.js
@@ -1,110 +1,149 @@
 import axios from "axios";
 import { ip } from "./request_host_ip";
 
-const get_user_st = async () => {
-    
-    const token = window.localStorage.getItem("access_token");
 
-    try {
-        const response = await axios.get(`${ip}/st`, {
-          headers: {
-            accept: "application/json",
-            Authorization: "Bearer " + token
-          },
-          validateStatus: function (status) {
-            return (status >= 200 && status < 300) || status === 404;
-          },
-        });
-        if (response.data) {
-          return response;
-        } else {
-          return false;
-        }
-      } catch (error) {
-        console.log(error);
-      }
+const get_user_st = async (access_token) => {
+  //  Get the current user's history Security Targets
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //  Return:
+  //    response: If sccessfully get the user's hisrtory st, return it. 
+  //    BOOL: Return false to show there's a problem happened when fetching st.
+
+  try {
+    const response = await axios.get(`${ip}/st`, {
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + token
+      },
+      validateStatus: function (status) {
+        return (status >= 200 && status < 300) || status === 404;
+      },
+    });
+    if (response.data) {
+      return response;
+    } else {
       return false;
+    }
+  } catch (error) {
+    return false;
+  }
 }
 export default get_user_st;
 
+
 export const update_username = async (access_token, new_username) => {
-    
+  // Update the user's username
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    new_username: The name the user want's to change to.
+  //  Return:
+  //    error.response.data.detail: The detail error information for frontend to show error message.
+  //    BOOL: Whether the update success or not.
+
   const body = {username: new_username}
 
   try {
-      const response = await axios.patch(`${ip}/user/update_username`, body, {
-        headers: {
-          accept: "application/json",
-          Authorization: "Bearer " + access_token
-        },
-        validateStatus: function (status) {
-          return (status >= 200 && status < 300) || status === 404;
-        },
-      });
+    const response = await axios.patch(`${ip}/user/update_username`, body, {
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + access_token
+      },
+      validateStatus: function (status) {
+        return (status >= 200 && status < 300) || status === 404;
+      },
+    });
       
-      if (response.status === 200) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (error) {
-      return error.response.data.detail;
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    return error.response.data.detail;
+  }
 }
 
+
 export const update_password = async (access_token, old_pass, new_pass) => {
-  
+  // Update the user's password
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    old_pass: The old password for user to check the identity once more.
+  //    new_pass: The new password the user want's to change to.
+  //  Return:
+  //    error.response.data.detail: The detail error information for frontend to show error message.
+  //    BOOL: Whether the update success or not.
+
   const body = {
     old_password: old_pass,
     new_password: new_pass
   }
 
   try {
-      const response = await axios.patch(`${ip}/user/update_password`, body, {
-        headers: {
-          accept: "application/json",
-          Authorization: "Bearer " + access_token
-        },
-        validateStatus: function (status) {
-          return (status >= 200 && status < 300) || status === 404;
-        },
-      });
+    const response = await axios.patch(`${ip}/user/update_password`, body, {
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + access_token
+      },
+      validateStatus: function (status) {
+        return (status >= 200 && status < 300) || status === 404;
+      },
+    });
       
-      if (response.status === 200) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (error) {
-      return error.response.data.detail;
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    return error.response.data.detail;
+  }
 }
 
+
 export const upload_st = async (access_token, st_file) => {
-  
+  // Uploading the new security target
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    st_file: The targeting security target file(pdf).
+  //  Return:
+  //    error.response.data.detail: The detail error information for frontend to show error message.
+  //    BOOL: Whether the upload success or not.
+  //    response.data: The detaial message for frontend to determine what actions to take.
+
   const formData = new FormData();
   formData.append('new_st', st_file);
 
   try {
-      const response = await axios.post(`${ip}/st/`, formData, {
-        headers: {
-          Authorization: "Bearer " + access_token
-        },
-        validateStatus: function (status) {
-          return (status >= 200 && status < 300) || status === 404;
-        },
-      });
-      if (response.data) {
-        return response.data;
-      } else {
-        return false;
-      }
-    } catch (error) {
-      return error.response.data.detail;
+    const response = await axios.post(`${ip}/st/`, formData, {
+      headers: {
+        Authorization: "Bearer " + access_token
+      },
+      validateStatus: function (status) {
+        return (status >= 200 && status < 300) || status === 404;
+      },
+    });
+    if (response.data) {
+      return response.data;
+    } else {
+      return false;
     }
+  } catch (error) {
+    return error.response.data.detail;
+  }
 }
 
+
 export const get_st_info = async (access_token, st_id) => {
+  // Get the current showing security target information
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    st_id: The targeting security target id.
+  //  Return:
+  //    BOOL: Whether the get request success or not.
+  //    response.data: The detaial message for frontend to determine what actions to take.
+
   try {
     const response = await axios.get(`${ip}/st/${st_id}`, {
       headers: {
@@ -121,13 +160,22 @@ export const get_st_info = async (access_token, st_id) => {
       return false;
     }
   } catch (error) {
-    console.log(error);
+    return false;
   }
-  return false;
 }
 
+
 export const st_evaluate = async (access_token, st_id, model, sesip_level) => {
-  console.log(model, sesip_level)
+  // Evaluate the targeting security target
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    st_id: The targeting security target id.
+  //    model: The LLM model the security target is going to evaluate with.
+  //    sesip_level: The desired SESIP level for the evaluation.
+  //  Return:
+  //    BOOL: Whether the evaluate success or not.
+  //    response.data: The detaial message for frontend to determine what actions to take.
+
   try {
     const response = await axios.post(`${ip}/eval/${st_id}`, null, {
       headers: {
@@ -148,12 +196,20 @@ export const st_evaluate = async (access_token, st_id, model, sesip_level) => {
       return false;
     }
   } catch (error) {
-    console.log(error);
+    return false;
   }
-  return false;
 }
 
+
 export const get_st_file_content = async (access_token, st_id) => {
+  // Get the security target file content (pdf file) for display
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    st_id: The targeting security target id.
+  //  Return:
+  //    BOOL: Whether the get request success or not.
+  //    response.data: The security target file blob data.
+
   try {
     const response = await axios.get(`${ip}/st/file/${st_id}`, {
       headers: {
@@ -171,12 +227,19 @@ export const get_st_file_content = async (access_token, st_id) => {
       return false;
     }
   } catch (error) {
-    console.log(error);
+    return false;
   }
-  return false;
 }
 
+
 export const delete_history_st = async (access_token, st_id) => {
+  // Delete the selected securty target evaluation data & files
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    st_id: The targeting security target id.
+  //  Return:
+  //    BOOL: Whether the delete success or not.
+
   try {
     const response = await axios.delete(`${ip}/st/${st_id}`, {
       headers: {
@@ -193,12 +256,20 @@ export const delete_history_st = async (access_token, st_id) => {
       return false;
     }
   } catch (error) {
-    console.log(error);
+    return false;
   }
-  return false;
 }
 
+
 export const get_st_report_download = async (access_token, st_id, toe_name) => {
+  // Delete the selected securty target evaluation data & files
+  //  Param: 
+  //    access_token: The token for doing user actions.
+  //    st_id: The targeting security target id.
+  //    toe_name: The TOE name to make evaluation result docx file.
+  //  Return:
+  //    BOOL: Whether the download success or not.
+
   try {
     const response = await axios.get(`${ip}/st/download/${st_id}`, {
       headers: {
@@ -213,26 +284,20 @@ export const get_st_report_download = async (access_token, st_id, toe_name) => {
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-
     const contentDisposition = response.headers['content-disposition'];
-    console.log( response)
     let fileName = st_id + '-' + toe_name + ' Evaluation Result.docx';
-
     if (contentDisposition && contentDisposition.indexOf('attachment') !== -1) {
       const fileNameMatch = contentDisposition.match(/filename\*?=['"]?([^;\n]+)['"]?/);
       if (fileNameMatch.length > 1) {
         fileName = decodeURIComponent(fileNameMatch[1].replace(/UTF-8''/, ''));
       }
     }
-
     link.setAttribute('download', fileName);
     document.body.appendChild(link);
     link.click();
-
     link.parentNode.removeChild(link);
     return true;
   } catch (error) {
-    console.log(error);
     return false;
   }
 }
