8eda40445a48469530792dc452d992ad07e4ef86
Lyz-0723 <LS0723000@gmail.com>
2024-08-12 16:26:47 +0800

    DOCS: Add comments to database.py

--- api/database.py
+++ api/database.py
@@ -17,14 +17,30 @@ import config
 
 # server.start()
 
+# Database settings
 DATABASE_URL = f"mysql+asyncmy://{config.db_user}:{config.db_pass}@{config.db_host}:{config.db_port}/{config.db_name}"
-
 db = databases.Database(DATABASE_URL)
 
 metadata = MetaData()
 
 
 async def execute_stmt_in_tran(stmt_list: list[ClauseElement]) -> bool:
+    """
+    Execute a list of SQL statements within a database transaction.
+
+    This asynchronous function performs the following steps:
+    - Starts a new database transaction.
+    - Executes each SQL statement in the provided `stmt_list` within the transaction.
+    - Commits the transaction if all statements are executed successfully.
+    - Rolls back the transaction and returns `False` if any statement fails.
+
+    Args:
+        stmt_list (list[ClauseElement]): A list of SQL statements to be executed.
+
+    Returns:
+        bool: `True` if all statements were executed and committed successfully, `False` otherwise.
+    """
+
     tran = db.transaction()
 
     try:
@@ -40,6 +56,22 @@ async def execute_stmt_in_tran(stmt_list: list[ClauseElement]) -> bool:
 
 
 async def create_with_result(stmt: ClauseElement) -> int:
+    """
+    Execute a single SQL Create statement within a database transaction and return the ID of the created item.
+
+    This asynchronous function performs the following steps:
+    - Starts a new database transaction.
+    - Executes the provided SQL statement (`stmt`) within the transaction.
+    - Commits the transaction if the statement is executed successfully.
+    - Rolls back the transaction and returns `0` if an error occurs.
+
+    Args:
+        stmt (ClauseElement): The SQL Create statement to be executed.
+
+    Returns:
+        int: The ID of the created item if successful; `0` if an error occurs.
+    """
+
     tran = db.transaction()
 
     try:
