d07bc651ca3cac38e983cd84e6de399a22be284a
Lyz-0723 <LS0723000@gmail.com>
2024-08-12 16:32:29 +0800

    DOCS: Add comments to config.py

--- api/config.py
+++ api/config.py
@@ -3,15 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Database configuration
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 db_user = os.getenv('DB_USER')
 db_pass = os.getenv('DB_PASS')
 db_name = os.getenv('DB_NAME')
 
+# JWT token configuration
 access_token_secret_key = os.getenv("ACCESS_TOKEN_SECRET_KEY")
 algorithm = os.getenv("ALGORITHM")
 access_token_expire_days = os.getenv("ACCESS_TOKEN_EXPIRE_DAYS")
 
+# Other configuration
 base_path = os.getenv("BASE_PATH")
 api_key = os.getenv("API_KEY")
