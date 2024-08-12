36135c00c9fa322e79ec6ccbb19a356d56b354c3
Lyz-0723 <LS0723000@gmail.com>
2024-08-12 11:00:57 +0800

    FEAT: Implement the feature to create the eval_file.docx

--- api/router/evaluate.py
+++ api/router/evaluate.py
@@ -7,6 +7,7 @@ from authentication.JWTtoken import get_current_user
 from exception import bad_request, no_such_st, st_not_belongs, eval_has_performed, evaluation_failed, invalid_model
 from repository.STCRUD import *
 from LLM.evaluation import evaluate
+from utils.file_process import generate_eval_report
 from schemas import EvaluateST
 from config import base_path
 
@@ -18,7 +19,8 @@ def copy_dict(dictionary, keys):
 
 
 @router.post("/{st_id}", status_code=status.HTTP_201_CREATED)
-async def evaluate_security_target(st_id: int, eval_model: str, current_user=Depends(get_current_user)) -> EvaluateST:
+async def evaluate_security_target(st_id: int, eval_model: str, sesip_level: int,
+                                   current_user=Depends(get_current_user)) -> EvaluateST:
     # Validation
     st = await get_st_by_id(st_id)
     if not st:
@@ -34,14 +36,16 @@ async def evaluate_security_target(st_id: int, eval_model: str, current_user=Dep
         raise invalid_model
 
     # Evaluation
-    evaluate(st_id, eval_model)
+    evaluate(st_id, eval_model, sesip_level)
 
     # Parsing
     try:
         dir_path = os.path.join(base_path, str(st_id))
         eval_results = json.load(open(os.path.join(dir_path, 'eval_result.json'), 'r', encoding='utf-8'))
 
-        st_details = copy_dict(eval_results, ["TOE_Name", "Developer_Organization", "SESIP_Level"])
+        st_details = copy_dict(eval_results, ["TOE_Name", "Developer_Organization"])
+        st_details["SESIP_Level"] = sesip_level
+
         eval_details = copy_dict(eval_results,
                                  ["Work_Units", "Work_Units_Evaluation_Result_Passes_Failed_Numbers_Status"])
         eval_passed = False if eval_results["Work_Units_Evaluation_Result_Passes_Failed_Numbers_Status"][1] else True
@@ -62,5 +66,10 @@ async def evaluate_security_target(st_id: int, eval_model: str, current_user=Dep
     # Update the data in db
     if not await update_st_after_eval(st_id, update_st):
         raise bad_request
+
+    try:
+        generate_eval_report(st_id, sesip_level)
+    except Exception as e:
+        print(f"Error: {e}")
 
     return update_st
---
+++ api/utils/file_process.py
@@ -0,0 +1,53 @@
+from docx import Document
+import os
+import json
+
+from config import base_path
+
+report_template = Document(os.path.join(base_path, "evaluation_report_template.docx"))
+
+level1 = {"ASE_INT.1-1": {}, "ASE_INT.1-2": {}, "ASE_INT.1-3": {}, "ASE_INT.1-4": {}, "ASE_INT.1-5": {},
+          "ASE_INT.1-6": {}, "ASE_INT.1-7": {}, "ASE_INT.1-8": {}, "ASE_INT.1-9": {}, "ASE_INT.1-10": {},
+          "ASE_INT.1-11": {}, "ASE_OBJ.1-1": {}, "ASE_REQ.3-1": {}, "ASE_REQ.3-2": {}, "ASE_REQ.3-3": {},
+          "ASE_REQ.3-4": {}, "ASE_REQ.3-5": {}, "ASE_REQ.3-6": {}, "ASE_REQ.3-7": {}, "ASE_TSS.1-1": {},
+          "ASE_TSS.1-2": {}, "ALC_FLR.2-1": {}, "ALC_FLR.2-2": {}, "ALC_FLR.2-3": {}, "ALC_FLR.2-4": {},
+          "ALC_FLR.2-5": {}, "ALC_FLR.2-6": {}, "ALC_FLR.2-7": {}, "ALC_FLR.2-8": {}, "ALC_FLR.2-9": {},
+          "ALC_FLR.2-10": {}}
+
+level_unit_mapping = {1: level1}
+
+
+def generate_eval_report(st_id: int, sesip_level: int):
+    save_path = os.path.join(base_path, str(st_id), "eval_file.docx")
+    details_path = os.path.join(base_path, str(st_id), "eval_details.json")
+    eval_details = json.load(open(details_path, "r"))
+
+    work_units = eval_details["Work_Units"]
+    mapping_unit = level_unit_mapping[sesip_level]
+
+    for unit in work_units:
+        mapping_unit[unit["Work_Unit_Name"]] = {
+            "description": unit["Work_Unit_Description"],
+            "status": unit["Work_Unit_Evaluation_Result_Status"]
+        }
+
+    p = report_template.paragraphs
+    for i in range(len(p)):
+        unit_name = p[i].text.split("(")[0].strip()
+        if unit_name in mapping_unit.keys():
+            try:
+                status = p[i + 2].add_run(mapping_unit[unit_name]["status"])
+                status.bold = True
+                status.font.name = "Times New Roman"
+
+                description = p[i + 3].add_run(mapping_unit[unit_name]["description"])
+                description.font.name = "Times New Roman"
+
+            except KeyError:
+                print(f"No such work unit: {unit_name}")
+                pass
+
+    report_template.save(save_path)
+
+
+generate_eval_report(11, 1)
